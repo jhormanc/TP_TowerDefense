3f2b83aee01cfc8c820567737331187b943c1160
Joris HORMANCEY <hmasterjo@hotmail.com>
2015-11-15 23:18:56 +0100

    Add effets
    Bullet TODO

--- Gamagora_Tower_Defense/Assets/Scripts/Weapons/Tourelle.cs
+++ Gamagora_Tower_Defense/Assets/Scripts/Weapons/Tourelle.cs
@@ -7,23 +7,25 @@ public class Tourelle : MonoBehaviour {
     public Transform Target;
     public float RotationSpeed;
     public float BulletSpeed;
-    public Rigidbody Bullet;
+    public GameObject Bullet;
 
     //values for internal use
     private Quaternion _lookRotation;
     private Vector3 _direction;
 
     private Vector3 _aim;
-    private bool _fire;
+    private bool _fire, _lastFire;
 
     // Use this for initialization
     void Start () {
         _aim = new Vector3();
         _fire = true;
+        EmitParticle(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        _fire = Input.GetKey(KeyCode.F);
         if (_fire)
         {
             Transform tourelle = transform.FindChild("Base").FindChild("Tourelle");
@@ -47,13 +49,32 @@ public class Tourelle : MonoBehaviour {
             _lookRotation = Quaternion.LookRotation(_direction);
 
             //rotate us over time according to speed until we are in the required rotation
-            tourelle.rotation = Quaternion.Slerp(tourelle.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+            //tourelle.rotation = Quaternion.Slerp(tourelle.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
     
             tourelle.FindChild("Canon").Rotate(0, 0, 20);
+            
 
             Transform shoot_point = tourelle.FindChild("Canon").FindChild("Shoot");
-            Rigidbody bullet = (Rigidbody)Instantiate(Bullet, shoot_point.position, shoot_point.rotation);
-            bullet.velocity = shoot_point.forward * BulletSpeed;
+            GameObject bullet = (GameObject)Instantiate(Bullet, shoot_point.position, shoot_point.rotation);
+            
+            bullet.GetComponentsInChildren<ParticleSystem>()[0].enableEmission = true;
+            bullet.GetComponent<Rigidbody>().velocity = shoot_point.forward * BulletSpeed;
+
         }
+
+        if (!_lastFire && _fire)
+            EmitParticle(true);
+        else if (_lastFire && !_fire)
+            EmitParticle(false);
+
+        _lastFire = _fire;
+    }
+
+    void EmitParticle(bool emit)
+    {
+        Transform p = transform.FindChild("Base").FindChild("Tourelle").FindChild("Particule");
+        p.GetComponent<ParticleSystem>().enableEmission = emit;
+        p.FindChild("Smoke").GetComponent<ParticleSystem>().enableEmission = emit;
+        p.FindChild("Sparks").GetComponent<ParticleSystem>().enableEmission = emit;
     }
 }
